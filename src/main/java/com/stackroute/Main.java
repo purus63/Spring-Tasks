b32f9fb2a71ec2e8497fe3c7a87fa7847b75dcc7
purus63 <2014dib021@gmail.com>
2019-10-05 13:57:23 +0530

    2nd Commit

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -4,33 +4,37 @@ import com.stackroute.domain.Movie;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionReader;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 
 
-/**
- * Hello world!
- *
- */
 public class Main
 {
     public static void main( String[] args )
     {
-
-          BeanFactory factory = new XmlBeanFactory(new ClassPathResource("bean.xml"));
+        //XMLBean Factory
+        BeanFactory factory = new XmlBeanFactory(new ClassPathResource("bean.xml"));
         Movie m1= factory.getBean("mov",Movie.class);
         m1.display();
 
-
-
+        //Application Context
         ApplicationContext context= new ClassPathXmlApplicationContext("bean.xml");
-        Movie m= context.getBean("mov",Movie.class);
-        m.display();
+        Movie m2= context.getBean("mov",Movie.class);
+        m2.display();
 
-
-
+        //BeanDefinitionRegistry
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        BeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+        reader.loadBeanDefinitions(new FileSystemResource("/home/cgi/Desktop/Spring/task1/src/main/resources/bean.xml"));
+        Movie m3 = beanFactory.getBean("mov",Movie.class);
+        m3.display();
 
     }
 }
